5bb5dbb76b32fe6ede6e01ae50a8f66383e4605c
PhuongDoan <95doanphuong@gmail.com>
2026-08-18 22:28:21 +0700

    test(03-01): add parser input-class matrix (RED on shorts + 12-char boundary)
    - Add tests for watch/mobile/youtu.be/embed/shorts/nocookie/non-leading-query URL forms
    - Add hyphen/underscore ID and non-YouTube/plain-text/empty-string nil-return tests
    - Add 10-char and 12-char ID-length boundary tests
    - shorts and 12-char boundary tests fail against the current regex (RED), fixed in the next commit

--- CarTubeTests/UtilitiesTests.swift
+++ CarTubeTests/UtilitiesTests.swift
@@ -15,4 +15,64 @@ final class UtilitiesTests: XCTestCase {
         XCTAssertEqual(extractYouTubeVideoID(url), "dQw4w9WgXcQ")
     }
 
+    func testExtractVideoIDFromMobileWatchURL() {
+        let url = "https://m.youtube.com/watch?v=dQw4w9WgXcQ"
+        XCTAssertEqual(extractYouTubeVideoID(url), "dQw4w9WgXcQ")
+    }
+
+    func testExtractVideoIDFromYoutuBeURL() {
+        let url = "https://youtu.be/dQw4w9WgXcQ"
+        XCTAssertEqual(extractYouTubeVideoID(url), "dQw4w9WgXcQ")
+    }
+
+    func testExtractVideoIDFromEmbedURL() {
+        let url = "https://www.youtube.com/embed/dQw4w9WgXcQ"
+        XCTAssertEqual(extractYouTubeVideoID(url), "dQw4w9WgXcQ")
+    }
+
+    func testExtractVideoIDFromShortsURL() {
+        let url = "https://www.youtube.com/shorts/dQw4w9WgXcQ"
+        XCTAssertEqual(extractYouTubeVideoID(url), "dQw4w9WgXcQ")
+    }
+
+    func testExtractVideoIDFromNonLeadingQueryParam() {
+        let url = "https://www.youtube.com/watch?list=PL123456789&v=dQw4w9WgXcQ"
+        XCTAssertEqual(extractYouTubeVideoID(url), "dQw4w9WgXcQ")
+    }
+
+    func testExtractVideoIDFromNoCookieEmbedURL() {
+        let url = "https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ"
+        XCTAssertEqual(extractYouTubeVideoID(url), "dQw4w9WgXcQ")
+    }
+
+    func testExtractVideoIDWithHyphenAndUnderscore() {
+        let url = "https://youtu.be/d-Qw4w9_gXc"
+        XCTAssertEqual(extractYouTubeVideoID(url), "d-Qw4w9_gXc")
+    }
+
+    func testExtractVideoIDReturnsNilForNonYouTubeURL() {
+        let url = "https://vimeo.com/12345"
+        XCTAssertNil(extractYouTubeVideoID(url))
+    }
+
+    func testExtractVideoIDReturnsNilForPlainText() {
+        let url = "hello world"
+        XCTAssertNil(extractYouTubeVideoID(url))
+    }
+
+    func testExtractVideoIDReturnsNilForEmptyString() {
+        let url = ""
+        XCTAssertNil(extractYouTubeVideoID(url))
+    }
+
+    func testExtractVideoIDReturnsNilForTenCharacterID() {
+        let url = "https://www.youtube.com/watch?v=dQw4w9WgXc"
+        XCTAssertNil(extractYouTubeVideoID(url))
+    }
+
+    func testExtractVideoIDReturnsNilForTwelveCharacterID() {
+        let url = "https://www.youtube.com/watch?v=dQw4w9WgXcQz"
+        XCTAssertNil(extractYouTubeVideoID(url))
+    }
+
 }
